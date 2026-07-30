884144896fd44c72bd644a75d005ec5f07a663a1
Itamar Shabtai <itamar.shabtai.1@gmail.com>
2026-07-31 01:44:27 +0300

    fmt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-
         ndk {
             if (abiFilterList == null) {
                 abiFilters.addAll(listOf("arm64-v8a"))
@@ -42,11 +41,11 @@ android {
 
     signingConfigs {
         val keyFile = file("androidkey.jks")
-	val storePasswordVal = System.getenv("SIGNING_STORE_PASSWORD") 
+        val storePasswordVal = System.getenv("SIGNING_STORE_PASSWORD")
             ?: localProperties.getProperty("SIGNING_STORE_PASSWORD")
-        val keyAliasVal = System.getenv("SIGNING_KEY_ALIAS") 
+        val keyAliasVal = System.getenv("SIGNING_KEY_ALIAS")
             ?: localProperties.getProperty("SIGNING_KEY_ALIAS")
-        val keyPasswordVal = System.getenv("SIGNING_KEY_PASSWORD") 
+        val keyPasswordVal = System.getenv("SIGNING_KEY_PASSWORD")
             ?: localProperties.getProperty("SIGNING_KEY_PASSWORD")
 
         if (keyFile.exists() && storePasswordVal != null && storePasswordVal.isNotEmpty()) {
