99bfda2aaa1dcc0071d99b222f9796e8e035f80e
Itamar Shabtai <itamar.shabtai.1@gmail.com>
2026-08-23 17:01:33 +0300

    add CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "itmr.dragonfable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 260720
-        versionName = "0.260720"
+        versionCode = 1
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
